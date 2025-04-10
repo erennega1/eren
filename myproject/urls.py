db8a1b01f256962464074e2b54fcd79dbb8b454d
Araiuly Eren <araiulyeren@gmail.com>
2025-04-10 22:13:50 +0500

    Обновление проекта

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from ads import views  
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ads/', include('ads.urls')),  
     path('users/', include('users.urls')),
     path('', views.ad_list, name='home'),  
-] 
-from django.conf import settings
-from django.conf.urls.static import static
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
